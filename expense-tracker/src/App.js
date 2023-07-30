be2c78127ddb0809cf69e196f061d17da380801b
akshay <akshaysuman129@gmail.com>
2023-07-30 23:45:24 +0530

    final

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,14 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Redirect, Navigate } from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Expense from "./components/Expense";
+
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/expense" element={<Expense />} />
       </Routes>
